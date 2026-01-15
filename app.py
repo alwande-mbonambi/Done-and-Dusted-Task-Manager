a5c3befb21eb8f88a71a58fe38561d4a2e50bc58
Demented-E <alwande.m18@gmail.com>
2026-01-15 22:04:39 +0200

    Postgre database setup

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from models import db, Todo
 from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///tasks.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -107,7 +108,9 @@ def clear_history():
     db.session.commit()
     return redirect('/')
 
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
